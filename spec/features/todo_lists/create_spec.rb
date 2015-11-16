4a2569e6fd21f1739d76188e2c48608ee9e98fdc
Pam Hiett <pamhiett@gmail.com>
2015-11-16 09:59:03 -0700

    Add presence and length for ToDo list description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -48,4 +48,40 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is the shopping I need to do today")
   end
+
+  it "displays an error when the todo list has no description" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Spring Cleaning"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Spring Cleaning")
+  end
+
+    it "displays an error when the todo list description is less than 7 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Spring Cleaning"
+    fill_in "Description", with: "spring"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Spring Cleaning")
+  end
 end
