d3a1010b908d72b9a5a9770f7f8a859d88beec66
Pam Hiett <pamhiett@gmail.com>
2015-11-18 16:22:19 -0700

    starting to tweak the styles to make the application look better

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -12,7 +12,7 @@ class TodoItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new(todo_items_params)
     if @todo_item.save
-      flash[:success] = "Added todo list item."
+      flash[:success] = "Congratulations: Your item was successfully added."
       redirect_to todo_list_todo_items_path
     else
       flash[:error] = "There was a problem adding that todo list item."
--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -15,11 +15,32 @@ describe "Adding todo items" do
     click_link "New Todo Item"
     fill_in "Content", with: "Milk"
     click_button "Save"
-    expect(page).to have_content("Added todo list item.")
+    expect(page).to have_content("Congratulations: Your item was successfully added.")
     within("ul.todo_items") do
       expect(page).to have_content("Milk")
     end
   end
 
+  it "displays an error with no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item.")
+    end
+    expect(page).to have_content("Content can't be blank")
+  end
+
+  it "displays an error with only 2 characters" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: "1"
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item.")
+    end
+    expect(page).to have_content("Content is too short (minimum is 2 characters")
+  end
 
 end
